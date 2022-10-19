1c2b3735e1d404be58444eab68d60d3cd03f5df3
Aloun <alounv@gmail.com>
2022-10-19 23:41:47 +0200

    Adds pre-commit

--- index.ts
+++ index.ts
@@ -1,21 +1,39 @@
 import { getAssignments, Preferences, ProjectPoints } from "./getAssignments";
 
 const projectsPoints: ProjectPoints = {
-  A: 2,
-  B: 5,
-  C: 3,
-  D: 1,
-  E: 3,
-  F: 5,
-  G: 3,
-  H: 2,
+  "24.03. Group edition side panel": 8,
+  "24.02. learner side panel + invitation date": 5,
+  "24.06. decide diffusion mode of publication": 5,
+  "24.01. add filters and search to groups and learners": 3,
+  "24.04. platform invitation email": 3,
+  "24.09. setup hotjar": 1,
 };
 
 const preferences: Preferences = {
-  Gary: ["A", "B", "C", "D", "E", "F", "G", "H"],
-  Aurelie: ["A", "B", "C", "D", "E", "F", "G", "H"],
-  Eddine: ["A", "B", "C", "D", "E", "F", "G", "H"],
-  Aloun: ["A", "B", "C", "D", "E", "F", "G", "H"],
+  Aurelie: [
+    "24.03. Group edition side panel",
+    "24.01. add filters and search to groups and learners",
+    "24.02. learner side panel + invitation date",
+    "24.04. platform invitation email",
+    "24.09. setup hotjar",
+    "24.06. decide diffusion mode of publication",
+  ],
+  Eddine: [
+    "24.01. add filters and search to groups and learners",
+    "24.02. learner side panel + invitation date",
+    "24.03. Group edition side panel",
+    "24.04. platform invitation email",
+    "24.06. decide diffusion mode of publication",
+    "24.09. setup hotjar",
+  ],
+  Aloun: [
+    "24.06. decide diffusion mode of publication",
+    "24.04. platform invitation email",
+    "24.03. Group edition side panel",
+    "24.02. learner side panel + invitation date",
+    "24.01. add filters and search to groups and learners",
+    "24.09. setup hotjar",
+  ],
 };
 
 const results = getAssignments(projectsPoints, preferences);
